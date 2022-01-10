16eabbb31aacf697940c065d16195d02bc88af94
Ashton94949 <97370406+Ashton94949@users.noreply.github.com>
2022-01-10 06:54:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ let ctx = canvas.getContext('2d');
 const GAME_WIDTH = 800;
 const GAME_HEIGTH = 600;
 
-ctx.clearRect(0, 0, 800, 600);
+
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
-
+fuction gameLoop() {
+ctx.clearRect(0, 0, 800, 600);
+  paddle.update();
+}
