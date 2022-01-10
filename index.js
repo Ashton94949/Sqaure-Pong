bdb0c0b5aafd98856dcff4cd3ed48fc48ce93b12
Ashton94949 <97370406+Ashton94949@users.noreply.github.com>
2022-01-10 07:16:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const GAME_HEIGTH = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
-fuction gameLoop() {
+let lastTime = 0;
+
+fuction gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
 ctx.clearRect(0, 0, 800, 600);
-  paddle.update();
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+  
+  requestAnimationFrame(gameLoop);
 }
